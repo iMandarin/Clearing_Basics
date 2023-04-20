1f46f8ab033383a666aef03810ebcf717b0bea16
iMandarin <odhamdhere@gmail.com>
2023-04-20 13:52:51 +0530

    conditionals and Loops Started

--- Code/src/Conditionals.java
+++ Code/src/Conditionals.java
@@ -1,8 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Conditionals
+{
     public static void main(String[] args)
     {
+
        /*
         Syntax for if statement:
         if (boolean True or False)
@@ -14,14 +16,40 @@ public class Main {
             //do this
         }
         */
-        int salary = 25400;
-        if (salary > 10000)
-        {
 
+        int salary = 15400;
+//        if (salary > 10000)
+//        {
+//            salary = salary + 2000;
+//        }
+//        else
+//        {
+//            salary = salary + 1000;
+//        }
+//        System.out.println(salary);
+
+
+        //multiple if-else statement
+        if (salary > 20000)
+        {
+            salary+=3000;
         }
+        else if (salary> 10000)
+        {
+            salary+=2000;
+        }
+        else
+        {
+            salary+=1000;
+        }
+        System.out.println(salary);
 
 
     }
+
+
+
+}
 //    public static void main(String[] args) {
 //        // Press Alt+Enter with your caret at the highlighted text to see how
 //        // IntelliJ IDEA suggests fixing it.
@@ -34,5 +62,3 @@ public class Main {
 //            // for you, but you can always add more by pressing Ctrl+F8.
 //            System.out.println("i = " + i);
 //        }
-
-}
