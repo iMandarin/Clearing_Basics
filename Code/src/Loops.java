89b2364b0f263cb1740ab2352af403d8f976f9d1
iMandarin <odhamdhere@gmail.com>
2023-04-21 19:56:07 +0530

    Started For Loop

--- Code/src/Loops.java
+++ Code/src/Loops.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Loops {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+
+        //Q. Print Numbers from 1 to 5
+        //One method
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+        // but this method is not possible if the number of iterations are more
+        // therefore, we will use loops.
+        //for loop
+
+        /*
+        Syntax of for loop:
+        for(initialisation; condition; increment/decrement)
+        {
+        //body
+        }
+         */
+        //now let's solve the above question again using for loop:
+
+//        System.out.println("Now, we will print numbers using for loop");
+//        for (int num =1; num <=5; num+=1)
+//        {
+//            System.out.println(num);
+//        }
+
+        //print numbers from 1 to n
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i <= n; i++)
+        {
+         //   System.out.println(i);
+            //suppose if I want to print the "Hello World!" N times then,
+            System.out.println("Hello World!");
+        }
+
+
     }
 }
