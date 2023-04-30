2c78073d83c13d08fdd04f3b6946c32fc74441b1
iMandarin <odhamdhere@gmail.com>
2023-05-01 00:12:15 +0530

    case check initial

---
+++ Code/src/CaseCheck.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+ //       char ch = in.next().trim().charAt(0);
+        String word = "hello";
+        System.out.println(word.charAt(0));
+
+//        System.out.println(ch);
+    }
+}
