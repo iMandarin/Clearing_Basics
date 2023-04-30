9511f073aa571cc9f4c857eb3f44a9c3a28b3624
iMandarin <odhamdhere@gmail.com>
2023-05-01 00:19:55 +0530

    case check initial

--- Code/src/CaseCheck.java
+++ Code/src/CaseCheck.java
@@ -4,10 +4,18 @@ public class CaseCheck {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
- //       char ch = in.next().trim().charAt(0);
-        String word = "hello";
-        System.out.println(word.charAt(0));
+        char ch = in.next().trim().charAt(0);
+        //String word = "hello";
+      //  System.out.println(word.charAt(2)); this will the character present at index 2. index starts at 0 therefore the O/P will be l.
 
-//        System.out.println(ch);
+        if (ch > 'a' && ch <= 'z')
+        {
+            System.out.println("LowerCase");
+        }
+        else
+        {
+            System.out.println("UpperCase");
+        }
+        System.out.println(ch);
     }
 }
